4ee1c367c454d59b7e46459d8eac3633726ee9e2
Soumya Bhandarkar <soumyabhandarkar2000@gmail.com>
2025-05-23 18:25:14 +0530

    Fix:  no of columns of table in mobile-view

--- components/FileCardsContainer.tsx
+++ components/FileCardsContainer.tsx
@@ -1,7 +1,26 @@
-export const FileCardsContainer = () => {
+import type { File as FileType } from "@/lib/db/schema";
+
+interface FileCardsContainerProps {
+  files: Array<FileType[]>;
+}
+
+export const FileCardsContainer = ({ files }: FileCardsContainerProps) => {
+  console.log(files);
+
   return (
-    <div className="p-4 border border-primary/40 rounded-lg w-full overflow-hidden">
-      <div className="relative flex flex-col gap-6 w-full h-full overflow-hidden"></div>
+    <div className="relative w-full overflow-auto p-4 border border-primary/40 rounded-lg ">
+      {/* <div className="w-[700px] overflow-x-auto"> */}
+      <div className="flex flex-col gap-6 h-full">
+        <div className="grid grid-cols-4 gap-2 p-2 bg-primary border-none hover:bg-primary rounded-lg">
+          <span className="w-full max-w-[200px] md:max-w-[250px] para-3 text-white text-center">
+            Name
+          </span>
+          <span className="para-3 text-white text-center">Type</span>
+          <span className="para-3 text-white text-center">Size</span>
+          <span className="para-3 text-white text-center">Updated On</span>
+        </div>
+      </div>
+      {/* </div> */}
     </div>
   );
 };
--- components/FilesContainer.tsx
+++ components/FilesContainer.tsx
@@ -21,6 +21,7 @@ import Link from "next/link";
 import { FullLoader } from "./FullLoader";
 import { FolderBreadCrumbs } from "./FolderBreadCrumbs";
 import { FileEmptyState } from "./FileEmptyState";
+import { FileCardsContainer } from "./FileCardsContainer";
 
 interface FileContProps {
   userId: string | null | undefined;
@@ -49,7 +50,6 @@ export const FilesContainer = ({
       }
 
       const response = await axios.get(url);
-      console.log(response);
 
       setFiles(response.data);
     } catch (error) {
@@ -68,7 +68,6 @@ export const FilesContainer = ({
     if (!userId) return;
 
     fetchFiles();
-    console.log("refresh val: ", refreshTrigger);
   }, [userId, refreshTrigger, currentFolder]);
 
   // Navigate to a folder
@@ -112,164 +111,167 @@ export const FilesContainer = ({
   };
 
   return (
-    <div className="p-4 border border-primary/40 rounded-lg w-full overflow-hidden">
+    <div className="p-4 border border-primary/40 rounded-lg w-full">
       {loading ? (
         <FullLoader />
       ) : (
-        <div className="flex flex-col gap-6 w-full h-full overflow-hidden">
+        <div className="relative flex flex-col gap-6 w-full h-full">
           <FolderBreadCrumbs
             folderPath={folderPath}
             navigateToPathFolder={navigateToPathFolder}
           />
 
-          <div className="relative w-[600px] max-w-[600px] md:max-w-full md:w-full h-[700px] max-h-[700px] overflow-x-auto overscroll-y-auto">
-            {files.length ? (
-              <Table className="min-w-[800px] table-fixed">
-                <TableHeader>
-                  <TableRow
-                    className="p-2 bg-primary border-none hover:bg-primary"
-                    // style={{ position: "sticky", top: "1rem" }}
-                  >
-                    <TableHead className="w-[200px] rounded-l-lg para-3 text-white px-3 text-center">
-                      Name
-                    </TableHead>
-                    <TableHead className="para-3 text-white text-center">
-                      Type
-                    </TableHead>
-                    <TableHead className="para-3 text-white text-center">
-                      Size
-                    </TableHead>
-                    <TableHead className="para-3 text-white text-center rounded-r-lg">
-                      Updated On
-                    </TableHead>
-                    {/* <TableHead className="rounded-r-lg para-3 text-white text-center">
-              Actions
-            </TableHead> */}
-                  </TableRow>
-                </TableHeader>
+          {/* <div className="w-full max-w-full"> */}
+          <div className="overflow-hidden">
+            {/* <FileCardsContainer files={[files]} /> */}
 
-                <TableBody>
-                  {files.map((file) => (
-                    <TableRow
-                      key={file.id}
-                      className="h-20 text-foreground border-primary/40"
-                    >
-                      <TableCell>
-                        <div
-                          className={`flex items-center gap-3 ${
-                            file.type === "folder" && "cursor-pointer"
-                          }`}
+            <>
+              {files.length ? (
+                <div className="relative w-full overflow-auto">
+                  <Table>
+                    <TableHeader>
+                      <TableRow className="p-2 bg-primary border-none hover:bg-primary">
+                        <TableHead className="w-[200px] rounded-l-lg para-3 text-white px-3 text-center">
+                          Name
+                        </TableHead>
+                        <TableHead className="para-3 text-white text-center rounded-r-lg min-[480px]:rounded-none">
+                          Type
+                        </TableHead>
+                        <TableHead className="hidden min-[480px]:table-cell min-[480px]:rounded-r-lg sm:rounded-none para-3 text-white text-center">
+                          Size
+                        </TableHead>
+                        <TableHead className="hidden sm:table-cell para-3 text-white text-center rounded-r-lg">
+                          Updated On
+                        </TableHead>
+                      </TableRow>
+                    </TableHeader>
+
+                    <TableBody>
+                      {files.map((file) => (
+                        <TableRow
+                          key={file.id}
+                          className="h-20 text-foreground border-primary/40"
                         >
-                          {file.type === "folder" ? (
-                            <TooltipProvider>
-                              <Tooltip>
-                                <TooltipTrigger asChild>
-                                  <Folder
-                                    className="min-w-5 min-h-5 w-5 h-5"
-                                    onClick={() => handleItemClick(file)}
-                                  />
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                  <p>Click to open folder</p>
-                                </TooltipContent>
-                              </Tooltip>
-                            </TooltipProvider>
-                          ) : (
-                            <>
-                              {file.type.startsWith("image") ? (
-                                <Image className="min-w-5 min-h-5 w-5 h-5" />
+                          <TableCell>
+                            <div
+                              className={`flex items-center gap-3 ${
+                                file.type === "folder" && "cursor-pointer"
+                              }`}
+                            >
+                              {file.type === "folder" ? (
+                                <TooltipProvider>
+                                  <Tooltip>
+                                    <TooltipTrigger asChild>
+                                      <Folder
+                                        className="min-w-5 min-h-5 w-5 h-5"
+                                        onClick={() => handleItemClick(file)}
+                                      />
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                      <p>Click to open folder</p>
+                                    </TooltipContent>
+                                  </Tooltip>
+                                </TooltipProvider>
                               ) : (
-                                <FileIcon className="min-w-5 min-h-5 w-5 h-5" />
+                                <>
+                                  {file.type.startsWith("image") ? (
+                                    <Image className="min-w-5 min-h-5 w-5 h-5" />
+                                  ) : (
+                                    <FileIcon className="min-w-5 min-h-5 w-5 h-5" />
+                                  )}
+                                </>
                               )}
-                            </>
-                          )}
-                          <div className="font-medium flex items-center gap-2 para-3">
-                            {file.type === "folder" ? (
-                              <TooltipProvider>
-                                <Tooltip>
-                                  <TooltipTrigger asChild>
-                                    <span
-                                      className="truncate max-w-[150px] sm:max-w-[200px] md:max-w-[250px]"
-                                      onClick={() => handleItemClick(file)}
-                                    >
-                                      {file.name}
-                                    </span>
-                                  </TooltipTrigger>
-                                  <TooltipContent>
-                                    <p>Click to open folder</p>
-                                  </TooltipContent>
-                                </Tooltip>
-                              </TooltipProvider>
-                            ) : (
-                              <span className="truncate max-w-[150px] sm:max-w-[200px] md:max-w-[250px]">
-                                {file.name}
+                              <div className="font-medium flex items-center gap-2 para-3">
+                                {file.type === "folder" ? (
+                                  <TooltipProvider>
+                                    <Tooltip>
+                                      <TooltipTrigger asChild>
+                                        <span
+                                          className="truncate max-w-[120px] sm:max-w-[180px] md:max-w-[220px]"
+                                          onClick={() => handleItemClick(file)}
+                                        >
+                                          {file.name}
+                                        </span>
+                                      </TooltipTrigger>
+                                      <TooltipContent>
+                                        <p>Click to open folder</p>
+                                      </TooltipContent>
+                                    </Tooltip>
+                                  </TooltipProvider>
+                                ) : (
+                                  <span className="truncate max-w-[150px] sm:max-w-[200px] md:max-w-[250px]">
+                                    {file.name}
+                                  </span>
+                                )}
+
+                                {file.type !== "folder" && (
+                                  <TooltipProvider>
+                                    <Tooltip>
+                                      <TooltipTrigger asChild>
+                                        <Link
+                                          href={file.fileUrl}
+                                          target="_blank"
+                                        >
+                                          <ExternalLink className="h-3 w-3 text-default-400" />
+                                        </Link>
+                                      </TooltipTrigger>
+                                      <TooltipContent>
+                                        <p>Click to view file</p>
+                                      </TooltipContent>
+                                    </Tooltip>
+                                  </TooltipProvider>
+                                )}
+                              </div>
+                            </div>
+                          </TableCell>
+
+                          <TableCell className="font-medium text-center">
+                            {file.type.startsWith("folder")
+                              ? file.type
+                              : file.type.split("/")[1]}
+                          </TableCell>
+
+                          <TableCell className="hidden min-[480px]:table-cell">
+                            <span className="flex items-center justify-center">
+                              {file.size === 0 ? (
+                                <Minus />
+                              ) : file.size < 1024 ? (
+                                `${file.size} B`
+                              ) : file.size < 1024 * 1024 ? (
+                                `${(file.size / 1024).toFixed(1)} KB`
+                              ) : (
+                                `${(file.size / (1024 * 1024)).toFixed(1)} MB`
+                              )}
+                            </span>
+                          </TableCell>
+
+                          <TableCell className="hidden sm:table-cell">
+                            <span className="flex flex-col items-center justify-center">
+                              <span>
+                                {file.updatedAt
+                                  ? new Intl.DateTimeFormat("en-US", {
+                                      timeStyle: "short",
+                                    }).format(new Date(file.updatedAt))
+                                  : "-"}
                               </span>
-                            )}
-
-                            {file.type !== "folder" && (
-                              <TooltipProvider>
-                                <Tooltip>
-                                  <TooltipTrigger asChild>
-                                    <Link href={file.fileUrl} target="_blank">
-                                      <ExternalLink className="h-3 w-3 text-default-400" />
-                                    </Link>
-                                  </TooltipTrigger>
-                                  <TooltipContent>
-                                    <p>Click to view file</p>
-                                  </TooltipContent>
-                                </Tooltip>
-                              </TooltipProvider>
-                            )}
-                          </div>
-                        </div>
-                      </TableCell>
-
-                      <TableCell className="font-medium text-center">
-                        {file.type.startsWith("folder")
-                          ? file.type
-                          : file.type.split("/")[1]}
-                      </TableCell>
-
-                      <TableCell>
-                        <span className="flex items-center justify-center">
-                          {file.size === 0 ? (
-                            <Minus />
-                          ) : file.size < 1024 ? (
-                            `${file.size} B`
-                          ) : file.size < 1024 * 1024 ? (
-                            `${(file.size / 1024).toFixed(1)} KB`
-                          ) : (
-                            `${(file.size / (1024 * 1024)).toFixed(1)} MB`
-                          )}
-                        </span>
-                      </TableCell>
-
-                      <TableCell>
-                        <span className="flex flex-col items-center justify-center">
-                          <span>
-                            {file.updatedAt
-                              ? new Intl.DateTimeFormat("en-US", {
-                                  timeStyle: "short",
-                                }).format(new Date(file.updatedAt))
-                              : "-"}
-                          </span>
-                          <span>
-                            {file.updatedAt
-                              ? new Intl.DateTimeFormat("en-US", {
-                                  dateStyle: "medium",
-                                }).format(new Date(file.updatedAt))
-                              : "-"}
-                          </span>
-                        </span>
-                      </TableCell>
-                      {/* <TableCell className="text-right">$250.00</TableCell> */}
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            ) : (
-              <FileEmptyState />
-            )}
+                              <span>
+                                {file.updatedAt
+                                  ? new Intl.DateTimeFormat("en-US", {
+                                      dateStyle: "medium",
+                                    }).format(new Date(file.updatedAt))
+                                  : "-"}
+                              </span>
+                            </span>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              ) : (
+                <FileEmptyState />
+              )}
+            </>
           </div>
         </div>
       )}
